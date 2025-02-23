e92583940b57b70efe1c679eb4715c6e753009c6
factfinding <970308389@qq.com>
2025-02-23 14:10:58 +0800

    change build branch

--- _config.ci.sh
+++ _config.ci.sh
@@ -18,7 +18,7 @@ fi
 # the tag/branch in the CI task definition
 DOTNET_VMR_CHECKED_OUT=false
 DOTNET_VMR_CHECKOUT="/vmr"
-DOTNET_VMR_BRANCH="main-9.x-loong"
+DOTNET_VMR_BRANCH="v9.0.101+loong.20250222"
 DOTNET_VMR_REPO=https://github.com/factfinding/dotnet.git
 
 ROOTFS_GLIBC_IMAGE_TAG="$(cat "$(dirname "${BASH_SOURCE[0]}")"/rootfs-glibc-image-tag.txt)"
